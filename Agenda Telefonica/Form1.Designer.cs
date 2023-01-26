11de0d8cf40f84645ce05c91a4e2d9427e54d1c9
your_name <your_address_email@example.com>
2023-01-26 09:25:12 +0100

    first commit

---
+++ Agenda Telefonica/Form1.Designer.cs
@@ -0,0 +1,424 @@
+﻿namespace Agenda_Telefonica
+{
+    partial class AgendaTeléfonica
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtTelePar = new System.Windows.Forms.TextBox();
+            this.txtTelemo = new System.Windows.Forms.TextBox();
+            this.txtComentario = new System.Windows.Forms.TextBox();
+            this.txtTeleTra = new System.Windows.Forms.TextBox();
+            this.txtExtension = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NombreApellidos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TelefonoParticular = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefonomovil = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TelefonoTrabajo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ExtensionTrabajo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CorreoElectronico = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Comentario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label8 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(155, 84);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 20);
+            this.txtNom.TabIndex = 0;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(155, 125);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(100, 20);
+            this.txtDireccion.TabIndex = 2;
+            // 
+            // txtTelePar
+            // 
+            this.txtTelePar.Location = new System.Drawing.Point(155, 168);
+            this.txtTelePar.Name = "txtTelePar";
+            this.txtTelePar.Size = new System.Drawing.Size(100, 20);
+            this.txtTelePar.TabIndex = 3;
+            // 
+            // txtTelemo
+            // 
+            this.txtTelemo.Location = new System.Drawing.Point(155, 208);
+            this.txtTelemo.Name = "txtTelemo";
+            this.txtTelemo.Size = new System.Drawing.Size(100, 20);
+            this.txtTelemo.TabIndex = 4;
+            // 
+            // txtComentario
+            // 
+            this.txtComentario.Location = new System.Drawing.Point(393, 208);
+            this.txtComentario.Name = "txtComentario";
+            this.txtComentario.Size = new System.Drawing.Size(100, 20);
+            this.txtComentario.TabIndex = 5;
+            // 
+            // txtTeleTra
+            // 
+            this.txtTeleTra.Location = new System.Drawing.Point(394, 84);
+            this.txtTeleTra.Name = "txtTeleTra";
+            this.txtTeleTra.Size = new System.Drawing.Size(100, 20);
+            this.txtTeleTra.TabIndex = 6;
+            // 
+            // txtExtension
+            // 
+            this.txtExtension.Location = new System.Drawing.Point(393, 129);
+            this.txtExtension.Name = "txtExtension";
+            this.txtExtension.Size = new System.Drawing.Size(100, 20);
+            this.txtExtension.TabIndex = 7;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(393, 168);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(100, 20);
+            this.txtCorreo.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nombre y apellidos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Dirección";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Teléfono Particular";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Teléfono móvil";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(300, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Teléfono Trabajo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(295, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Extensión trabajo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(295, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Correo electrónico";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(295, 215);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Comentarios";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(293, 28);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Datos Personales";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(577, 28);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 20;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(16, 57);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 21;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(97, 57);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 22;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNuevo);
+            this.groupBox1.Controls.Add(this.btnModificar);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Location = new System.Drawing.Point(628, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 163);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opciones a Realizar";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(97, 86);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 25;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(16, 86);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 23;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(40, 257);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(888, 292);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Registro";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NombreApellidos,
+            this.Direccion,
+            this.TelefonoParticular,
+            this.Telefonomovil,
+            this.TelefonoTrabajo,
+            this.ExtensionTrabajo,
+            this.CorreoElectronico,
+            this.Comentario});
+            this.dataGridView1.Location = new System.Drawing.Point(35, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(814, 206);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+            // 
+            // NombreApellidos
+            // 
+            this.NombreApellidos.HeaderText = "NombreApellidos";
+            this.NombreApellidos.Name = "NombreApellidos";
+            // 
+            // Direccion
+            // 
+            this.Direccion.HeaderText = "Direccion";
+            this.Direccion.Name = "Direccion";
+            // 
+            // TelefonoParticular
+            // 
+            this.TelefonoParticular.HeaderText = "TelefonoParticular";
+            this.TelefonoParticular.Name = "TelefonoParticular";
+            // 
+            // Telefonomovil
+            // 
+            this.Telefonomovil.HeaderText = "Telefonomovil";
+            this.Telefonomovil.Name = "Telefonomovil";
+            // 
+            // TelefonoTrabajo
+            // 
+            this.TelefonoTrabajo.HeaderText = "TelefonoTrabajo";
+            this.TelefonoTrabajo.Name = "TelefonoTrabajo";
+            // 
+            // ExtensionTrabajo
+            // 
+            this.ExtensionTrabajo.HeaderText = "ExtensionTrabajo";
+            this.ExtensionTrabajo.Name = "ExtensionTrabajo";
+            // 
+            // CorreoElectronico
+            // 
+            this.CorreoElectronico.HeaderText = "CorreoElectronico";
+            this.CorreoElectronico.Name = "CorreoElectronico";
+            // 
+            // Comentario
+            // 
+            this.Comentario.HeaderText = "Comentario";
+            this.Comentario.Name = "Comentario";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(336, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(176, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Click en el dato a Modificar/Eliminar";
+            // 
+            // AgendaTeléfonica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 590);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtExtension);
+            this.Controls.Add(this.txtTeleTra);
+            this.Controls.Add(this.txtComentario);
+            this.Controls.Add(this.txtTelemo);
+            this.Controls.Add(this.txtTelePar);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtNom);
+            this.Name = "AgendaTeléfonica";
+            this.Text = "AgendaTeléfonica";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtTelePar;
+        private System.Windows.Forms.TextBox txtTelemo;
+        private System.Windows.Forms.TextBox txtComentario;
+        private System.Windows.Forms.TextBox txtTeleTra;
+        private System.Windows.Forms.TextBox txtExtension;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+      
+        private System.Windows.Forms.Button btnNuevo;
+        public System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreApellidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TelefonoParticular;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefonomovil;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TelefonoTrabajo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ExtensionTrabajo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CorreoElectronico;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comentario;
+    }
+}
+
---
+++ Agenda Telefonica/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda_Telefonica
+{
+    public partial class AgendaTeléfonica : Form
+    {
+        //creamos una variable global que captura el evento de ser click en la tabla(dataGridView1)
+        int poc;
+        public AgendaTeléfonica()
+        {
+            InitializeComponent();
+            limpiar();
+        }
+
+        //metodo para limpiar el textBox cuando agregamos nueva informacion
+        private void limpiar()
+        {
+            txtNom.Clear();
+            txtDireccion.Clear();
+            txtComentario.Clear();
+            txtCorreo.Clear();
+            txtExtension.Clear();
+            txtTelemo.Clear();
+            txtTelePar.Clear();
+            txtTeleTra.Clear();
+            //desabilita los botones Modificar/Eliminar
+            btnModificar.Enabled = false;
+            btnEliminar.Enabled = false;
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        //metodo para agregar al presionar el boton
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //capturamos los campos de textBox
+            string n, d, t, f, m, q, w, l;
+            n = txtNom.Text;
+            d = txtDireccion.Text;
+            t = txtTelePar.Text;
+            f = txtTelemo.Text;
+            m = txtTeleTra.Text;
+            q = txtExtension.Text;
+            w = txtCorreo.Text;
+            l = txtComentario.Text;
+           
+            //agregamos los datos en una fila de la tabla
+            dataGridView1.Rows.Add( n, d,t, f, m, q, w, l);
+            //llamamos el metodo limpiar 
+            limpiar();
+            
+           
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //eliminamos una sola fila
+            dataGridView1.Rows.RemoveAt(poc);
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+        //metodo que vuelve a la normalidad(no modifica o elimina)
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            limpiar();
+            //habilitamos el boton agregar
+            btnAgregar.Enabled = true;
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            //capturamos los datos
+            string n, d, t, f, m, q, w, l;
+            n = txtNom.Text;
+            d = txtDireccion.Text;
+            t = txtTelePar.Text;
+            f = txtTelemo.Text;
+            m = txtTeleTra.Text;
+            q = txtExtension.Text;
+            w = txtCorreo.Text;
+            l = txtComentario.Text;
+
+            //modificamos las columnas de la tabla(dataGridView1)
+            dataGridView1[0, poc].Value = n;
+            dataGridView1[1, poc].Value = d;
+            dataGridView1[2, poc].Value = t;
+            dataGridView1[3, poc].Value = f;
+            dataGridView1[4, poc].Value = m;
+            dataGridView1[5, poc].Value = q;
+            dataGridView1[6, poc].Value = w;
+            dataGridView1[7, poc].Value = l;
+        
+
+            limpiar();
+        }
+
+        //creamos el metodo para capturar el click en la tabla(dataGridView1)
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            //captura la posicion en la tabla
+            poc = dataGridView1.CurrentRow.Index;
+            //captura los textBox y le asignamos un valor
+            txtNom.Text = dataGridView1[0, poc].Value.ToString();
+            txtDireccion.Text = dataGridView1[1, poc].Value.ToString();
+            txtTelePar.Text = dataGridView1[2, poc].Value.ToString();
+            txtTelemo.Text = dataGridView1[3, poc].Value.ToString();
+            txtTeleTra.Text = dataGridView1[4, poc].Value.ToString();
+            txtExtension.Text = dataGridView1[5, poc].Value.ToString();
+            txtCorreo.Text = dataGridView1[6, poc].Value.ToString();
+            txtComentario.Text = dataGridView1[7, poc].Value.ToString();
+
+            //abilitamos los botones Modificar/Eliminar y desabilitamos Agregar para que no se inserte un dato
+            btnAgregar.Enabled = false;
+            btnModificar.Enabled = true;
+            btnEliminar.Enabled = true;
+            
+
+        }
+    }
+}
